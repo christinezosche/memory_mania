77040e9d518924e69e1af60c20434dddaa6644d9
christinezosche <christine.zosche@gmail.com>
2021-01-26 11:19:08 -0800

    Updated styling elements

--- memory-mania-frontend/src/Game.js
+++ memory-mania-frontend/src/Game.js
@@ -18,14 +18,11 @@ class Game extends Component {
     shuffle = (array) => {
         let currentIndex = array.length, temporaryValue, randomIndex;
       
-        // While there remain elements to shuffle...
         while (0 !== currentIndex) {
       
-          // Pick a remaining element...
           randomIndex = Math.floor(Math.random() * currentIndex);
           currentIndex -= 1;
       
-          // And swap it with the current element.
           temporaryValue = array[currentIndex];
           array[currentIndex] = array[randomIndex];
           array[randomIndex] = temporaryValue;
@@ -40,7 +37,6 @@ class Game extends Component {
 
         return (
         <div className="gameContainer">
-                {/* {randomizedArray.map((image) => <GameCard imageUrl={image.url} imageId={image.id}/>)} */}
                 {randomizedArray.map((image) => <GameCard imageUrl={image.url} imageId={image.id}/>)}
         </div>
         )
