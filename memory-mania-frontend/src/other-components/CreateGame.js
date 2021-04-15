8f4fdfa0d4dbc12b34e4f2367c1e17ad1a31406b
christinezosche <christine.zosche@gmail.com>
2021-04-15 14:16:09 -0700

    Cleaned up unnecessary code

--- memory-mania-frontend/src/other-components/CreateGame.js
+++ memory-mania-frontend/src/other-components/CreateGame.js
@@ -6,7 +6,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Input from './Input'
 
 class CreateGame extends Component {
 
